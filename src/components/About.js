c5a6543b3a3c20dea73248d24db2fcbd6957a0c5
AngeliniAxel <axel.angelini@gmail.com>
2024-07-29 01:28:54 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,83 @@ const About = () => {
         <i class='fas fa-address-card'></i> CAREER PROFILE
       </h1>
       <p className='about-info'>
-      I´m a full-stack developer, and despite not having experience working in that field yet, I
-        consider myself a person with a great ability to acquire new knowledge,
-        through practice and learning from colleagues who have more experience
-        than me. I have no doubt that if you give me the opportunity to prove
-        it, I will be able to adapt quickly to the way you work at your company.
-        I have experience in Javascript (ES6), React, Redux, Node.js,
-        Express.js, Postgresql, HTML5, CSS3, responsive design, Bootstrap, and
-        I’m constantly learning. I can speak fluently in English. I enjoy
-        working in a team and learning new things every day. Outside the scope
-        of programming, I use my free time to work out, play videogames or read
-        comics.
+        I’m a Full Stack developer specialized in JavaScript (JS - TS - React -
+        Node - PostgreSQL) looking for an opportunity to prove that I'm a hard
+        worker, passionate about learning new things every day and overcoming my
+        limits.
+      </p>
+      <p className='about-info'>
+        While I'm looking for my first developer job, I'm also studying an
+        advanced course focused mostly on Front-End development at
+        digitalhouse.com to further enhance my skills. This course covers
+        advanced React-Redux with TypeScript, design patterns with Styled
+        Components, Next.js, testing with Jest and Cypress, scalable database
+        architectures with SQL and MongoDB, and automated testing.
+      </p>
+      <p className='about-info'>
+        I studied for two years in a programming degree at UTN (Universidad
+        Tecnológica Nacional, Buenos Aires) and then improved my skills through
+        online courses (mostly a full-stack course on codecademy.com). I have
+        experience in JavaScript (ES6), TypeScript, React, Redux, Node.js,
+        Express.js, PostgreSQL, HTML5, CSS3, responsive design, Bootstrap, and
+        every day I try to learn new things.
+      </p>
+      <p className='about-info'>
+        During the last year, I didn't actively seek programming-related jobs
+        because I dedicated myself to traveling in Australia and improving my
+        English proficiency. Despite that, I always used my free time to
+        continue learning. Nowadays, I find myself more settled, living in
+        Menorca, and seeking a company where I can learn and grow.
+      </p>
+      <p className='about-info'>
+        I consider myself a quick learner who benefits greatly from teamwork
+        alongside colleagues with more experience than me, and I have no doubt
+        that I can adapt very quickly to your way of working.
+      </p>
+      <p className='about-info'>
+        Outside the scope of programming, I use my free time to work out, play
+        videogames or read comics.
       </p>
       <h1 className='about-title'>
         <i class='fas fa-university'></i> EDUCATION
       </h1>
+      <div className='meta'>
+        <div className='upper-row'>
+          <h3 className='job-title'>Advanced Front-End Development</h3>
+          <div className='job-date'>2024</div>
+        </div>
+
+        <div className='job-place'>
+          <a
+            className='DigitalHouse'
+            href='https://www.digitalhouse.com/ar/productos/programacion/front-end-developer'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Codecademy
+          </a>
+        </div>
+        <br />
+      </div>
+
+      <div className='meta'>
+        <div className='upper-row'>
+          <h3 className='job-title'>Full-Stack Engineer</h3>
+          <div className='job-date'>2020 - 2021</div>
+        </div>
+
+        <div className='job-place'>
+          <a
+            className='Codecademy'
+            href='https://www.codecademy.com/learn/paths/full-stack-engineer-career-path'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Codecademy
+          </a>
+        </div>
+      </div>
+
       <div className='meta'>
         <div className='upper-row'>
           <h3 className='job-title'>Programming Technician</h3>
@@ -32,28 +94,7 @@ const About = () => {
         <br />
       </div>
 
-      <div className='job-detail'>
-      </div>
-      <div className='meta'>
-        <div className='upper-row'>
-          <h3 className='job-title'>Full-Stack Engineer</h3>
-          <div className='job-date'>2020 - 2021</div>
-        </div>
-
-        <div className='job-place'>
-          <a
-            className='codecademy'
-            href='https://www.codecademy.com'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Codecademy
-          </a>
-        </div>
-      </div>
-
-      <div className='job-detail'>
-      </div>
+      <div className='job-detail'></div>
     </div>
   );
 };
